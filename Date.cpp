09b5fec105a42a6b04fb16cccc184daa76d5644f
theProgrammingBox <justinboxgea@gmail.com>
2022-06-16 21:36:42 -0700

    idk

--- Date.cpp
+++ Date.cpp
@@ -11,10 +11,6 @@ ostream &operator<<(ostream &os, const Date &d)
 {
 	os << d.month << "/" << d.day << "/" << d.year << endl;
 	return os;
-<<<<<<< HEAD
 }
 
-// changes
-=======
-}
->>>>>>> 2636778cd1c9b30fe4880bcae3b04aecb5ebfe04
+// Language: cpp
