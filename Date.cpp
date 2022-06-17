db77967aedadd9083234b40a5ccc1f3d1452c20b
theProgrammingBox <justinboxgea@gmail.com>
2022-06-16 21:23:10 -0700

    test

--- Date.cpp
+++ Date.cpp
@@ -6,3 +6,9 @@ void Date::printDate() const
 {
 	cout << month << "/" << day << "/" << year << endl;
 }
+
+ostream &operator<<(ostream &os, const Date &d)
+{
+	os << d.month << "/" << d.day << "/" << d.year << endl;
+	return os;
+}
